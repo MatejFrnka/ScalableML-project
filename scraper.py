18012cdf14aeb944c772107978789615954a014e
Matěj Frnka <mat.frnka@gmail.com>
2022-12-16 16:48:08 +0100

    updating scraper

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import math
 import urllib
 
 import pandas as pd
@@ -9,6 +10,19 @@ FOOTBALL_DATA_LEAGUES = ["EC", "E3", "E2", "E1", "E0"]
 
 
 def from_football_data():
+    def normalize_date(str_val):
+        if type(str_val) != str and math.isnan(str_val):
+            return None
+        vals = str_val.split("/")
+        assert len(vals) == 3
+        year = vals[-1]
+        if len(year) == 2:
+            if int(year) < 70:
+                year = "20" + year
+            else:
+                year = "19" + year
+        return f"{vals[0]}/{vals[1]}/{year}"
+
     result = pd.DataFrame()
     for year in FOOTBALL_DATA_YEARS:
         for league in FOOTBALL_DATA_LEAGUES:
@@ -22,6 +36,9 @@ def from_football_data():
                 print(f"Unknown error: {url}")
                 print(e)
                 continue
+            t["Date"] = t["Date"].apply(normalize_date)
+            t["Date"] = pd.to_datetime(t["Date"], format="%d/%m/%Y")
+            t["Season"] = f"{str(year).zfill(2)}{str((year + 1) % 100).zfill(2)}"
             result = pd.concat([t, result])
             print(f"Downloaded: {url}")
 
